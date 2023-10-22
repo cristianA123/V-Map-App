5e5d2557415e39b08d08ee6eded874f5cd61a247
cristianA123 <cristianchipan2@gmail.com>
2023-10-21 22:41:11 -0500

    crear componente de busqueda y debounce

---
+++ src/components/search-results/SearchResults.vue
@@ -0,0 +1,31 @@
+<script src="./SearchResults" lang="ts"></script>
+
+<template>
+   <ul class="list-group mt-3">
+            <li class="list-group-item list-group-item-action">
+                <h5>Nombre del lugar</h5>
+                <p>Lorem ipsum dolor sit aperiam ab, ullam natus reiciendis culpa.</p>
+                <div align="right">
+                    <button class="btn btn-outline-primary btn-sm">
+                        Direcciones
+                    </button>
+                </div>
+            </li>
+        </ul>
+</template>
+
+
+<style scoped>
+
+li {
+    cursor: pointer;
+}
+h5 {
+    font-size: 15px !important;
+}
+
+p {
+    font-size: 12px !important;
+}
+
+</style>
---
+++ src/components/searchbar/SearchBar.vue
@@ -0,0 +1,34 @@
+<script src="./SearchBar" lang="ts"></script>
+
+<template>
+    <div class="searchbar-container">
+        <input 
+            v-model="searchTerm"
+            class="form-control"
+            type="text"
+            placeholder="Buscar lugares..."
+        />
+
+        <SearchResults />
+    </div>
+</template>
+
+
+
+<style scoped>
+
+.searchbar-container {
+    position: fixed;
+    top: 30px;
+    left: 30px;
+    background-color: white;
+    z-index: 999;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    width: 300px;
+    border-radius:  10px !important;
+    overflow: hidden;
+    padding: 5px;
+}
+
+
+</style>
